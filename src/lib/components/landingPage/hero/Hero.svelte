51c4742cc71ad8a4b2b3ac218a30ac329a30b3c6
Adetule-ayomide <adetuleayomide@gmail.com>
2024-02-16 04:28:34 +0100

    hero section

--- src/lib/components/landingPage/hero/Hero.svelte
+++ src/lib/components/landingPage/hero/Hero.svelte
@@ -1,10 +1,25 @@
 <script>
 	import * as Alert from '$lib/components/ui/alert';
 	import { AlertCircle } from 'lucide-svelte';
+	import { onDestroy, onMount } from 'svelte';
+	
+	let imgSrc = 'public/images/img.jpg';
+
+	onMount(() => {
+
+		let interval = setInterval(() => {
+			imgSrc = imgSrc === 'public/images/cv.png' ? 'public/images/img.jpg' : 'public/images/cv.png';
+		}, 3000);
+
+		onDestroy(() => {
+			clearInterval(interval);
+		});
+  });
+  
 </script>
 
 <section class="relative h-full">
-	<div class="maximum w-full">
+	<div class="maximum w-full h-full flex">
 		<div class="hero-texts">
 			<Alert.Root class="w-max max-w-lg">
 				<Alert.Title class="flex items-center gap-2">
@@ -18,22 +33,72 @@
 				>
 			</Alert.Root>
 
-			<div>
-				<h1>Lorem, ipsum.</h1>
-				<div>
-					<div>
-						<h3>Lorem ipsum dolor sit.</h3>
-						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-						<a href="/">Lorem, ipsum.</a>
+			<div class="text-5xl font-bold uppercase mt-10">
+				<p>build your cv</p>
+			</div>
+
+			<div class="flex gap-14 mt-20">
+				<div class="w-58">
+					<p class="text-xl capitalize mt-4 font-bold">Lorem ipsum dolor sit.</p>
+
+					<p class="text-sm mt-4">Lorem ipsum dolor sit amet sectetur adipisicing elit.</p>
+
+					<div class="capitalize underline mt-10 font-bold">
+						<a href=" ">get started</a>
 					</div>
-					<div>
-						<h3>Lorem ipsum dolor sit.</h3>
-						<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-						<a href="/">Lorem, ipsum.</a>
+
+				</div>
+
+				<div class="w-58">
+					<p class="text-xl capitalize mt-4 font-bold">ipsum dolor sit.</p>
+
+					<p class="text-sm mt-4">Lorem ipsum dolor sit amet sectetur adipisicing elit.</p>
+
+					<div class="capitalize underline mt-10 font-bold">
+						<a href=" ">get started</a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="hero-image bg-primary w-full h-full"></div>
+
+		<div class="hero-image bg-primary w-full h-full min-h-[500px] ml-16 relative">
+
+			<img src={imgSrc} alt="profile img" class="img custom-pulse h-60 w-64 ml-32 mt-52 animate-pulse">
+
+			<div class="w-48 h-48 pl-2 absolute top-28 -left-24">
+				<div class="w-44 h-12 py-3 bg-black text-white text-center rounded-lg absolut animate-bounce">
+					<p>Lorem, ipsum dolor.</p>
+				</div>
+
+				<div class="w-44 h-12 py-3 bg-black text-white text-center rounded-lg absolute top-14">
+					<p>Lorem, ipsum.</p>
+				</div>
+
+				<div class="w-44 h-12 py-3 bg-black text-white text-center rounded-lg absolute bottom-8">
+					<p>Lorem, ipsum ygiu.</p>
+				</div>
+			</div>
+
+			<div>
+				<div class="w-56 h-30 py-3 bg-black text-white text-center rounded-lg absolute right-10 bottom-16 animate-bounce">
+					<p class="font-bold">Lorem, ipsum dolor.</p>
+					<p class="text-sm text-gray-500">Lorem, ipsum.</p>
+					<p class="text-sm font-light">Lorem, ipsum dolor.</p>
+				</div>
+
+				<div class="custom-pulse w-56 h-30 py-3 bg-black text-white text-center rounded-lg absolute right-10 -bottom-12 animate-pulse">
+					<p class="font-bold">Lorem, ipsum dolor.</p>
+					<p class="text-sm text-gray-500">Lorem, ipsum.</p>
+					<p class="text-sm font-light">Lorem, ipsum dolor.</p>
+
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
+
+<style>
+	.custom-pulse {
+    animation: pulse 10s infinite;
+  }
+</style>
