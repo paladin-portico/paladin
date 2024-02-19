85189c3210b86cf917840cdbae9c7e48503ef1c9
Adetule-ayomide <adetuleayomide@gmail.com>
2024-02-19 19:55:41 +0100

    features page

--- src/lib/components/features/FeatureGrid.svelte
+++ src/lib/components/features/FeatureGrid.svelte
@@ -1,26 +1,54 @@
 <section class="maximum gridbox mt-10">
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
-	<div class="card"></div>
+	<div class="card">
+		<p class="h-full flex justify-center items-center">Lorem, ipsum dolor.</p>
+	</div>
+	<div class="card">
+		<img src="public/images/img.jpg" alt="" class="w-[100%] h-[60%]" />
+		<div class="mt-6 p-6">
+			<p>Lorem, ipsum.</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, iure!</p>
+		</div>
+	</div>
+	<div class="card">
+		<img src="public/images/img.jpg" alt="" class="w-[100%] h-[50%]" />
+		<div class="text-white mt-6 p-6">
+			<p>Lorem, ipsum.</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, iure!</p>
+		</div>
+	</div>
+	<div class="card">
+		<p class="h-full flex justify-center items-center">Lorem ipsum dolor sit.</p>
+	</div>
+	<div class="card">
+		<p class="py-10 p-6">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, deserunt.
+		</p>
+	</div>
+	<div class="card flex">
+		<img src="public/images/img.jpg" alt="" class="w-[50%]" />
+		<div class="text-white mt-6 p-6">
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, quia.</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, beatae?</p>
+		</div>
+	</div>
 </section>
 
 <style lang="postcss">
 	.gridbox {
 		width: 100%;
-		height: 600px;
+		/* height: 600px; */
 		display: grid;
 		grid-template-columns: repeat(3, 1fr);
 		grid-template-rows: 2fr, 1fr, 1fr;
 		gap: 1rem;
+		box-sizing: border-box;
 	}
 
 	.card {
 		width: 100%;
 		height: 100%;
 		background: #ff4d00;
+		box-sizing: border-box;
 	}
 
 	.card:nth-of-type(1) {
