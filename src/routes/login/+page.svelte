7a5e1402a924443a27315f4418b04b38f0827d53
Adetule-ayomide <adetuleayomide@gmail.com>
2024-02-25 20:38:14 +0100

    auth

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+</script>
+
+<section class="maximum w-full flex flex-col justify-center items-center">
+	<div class="w-[40%] h-full my-4">
+		<div class="welcome">
+			<h1 class="text-4xl font-semibold mb-2">welcome back</h1>
+			<p>welcome back! please enter your details.</p>
+		</div>
+		<form class="my-4">
+			<div class="my-2">
+				<label class="capitalize font-semibold" for="email">email</label>
+				<input
+					class="w-[90%] py-[6px] px-2 mt-2 block border rounded"
+					type="email"
+					name="email"
+					id="email"
+					placeholder=" Enter your email"
+				/>
+			</div>
+			<div>
+				<label class="capitalize font-semibold" for="password">password</label>
+				<input
+					class="w-[90%] py-[6px] px-2 mt-2 block border rounded"
+					type="password"
+					id="password"
+					placeholder="**********"
+				/>
+			</div>
+
+			<div class="w-[90%] flex justify-between my-4">
+				<div>
+					<input type="checkbox" id="check" />
+					<label for="check">Remember for 30 days</label>
+				</div>
+				<a href=" " class="text-primary">forget password</a>
+			</div>
+			<button
+				class="w-[90%] border py-[6px] rounded mb-4 bg-primary text-white font-semibold hover:opacity-[80%]"
+				>Sign in</button
+			>
+			<button class="w-[90%] border py-[6px] rounded mb-4 font-semibold hover:opacity-[80%]"
+				>Sign in with Google</button
+			>
+
+			<div class="text-center">
+				<p>Don't have an account? <span class="text-primary"><a href=" ">Sign up</a></span></p>
+			</div>
+		</form>
+	</div>
+</section>
+
+<style>
+</style>
---
+++ src/routes/sign-up/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+</script>
+
+<section class="maximum w-full flex flex-col justify-center items-center">
+	<div class="w-[40%] h-full my-4">
+		<div class="welcome w-[90%] text-center">
+			<h1 class="text-3xl font-semibold mb-2 capitalize">sign up</h1>
+			<p>Create your account</p>
+		</div>
+		<form class="w-[90%]">
+			<div class="my-2">
+				<label class="capitalize font-semibold" for="username">username</label>
+				<input
+					class="w-full py-[6px] px-2 mt-2 block border rounded"
+					type="text"
+					name="username"
+					id="username"
+					placeholder=" Enter your Username"
+				/>
+			</div>
+			<div class="my-2">
+				<label class="capitalize font-semibold" for="email">email</label>
+				<input
+					class="w-full py-[6px] px-2 mt-2 block border rounded"
+					type="email"
+					id="email"
+					placeholder="Enter your email"
+				/>
+			</div>
+			<div class="my-4">
+				<label class="capitalize font-semibold" for="password">password</label>
+				<input
+					class="w-full py-[6px] px-2 mt-2 block border rounded"
+					type="password"
+					id="password"
+					placeholder="*********"
+				/>
+			</div>
+
+			<button
+				class="w-full border py-[6px] rounded mb-2 bg-primary text-white font-semibold hover:opacity-[80%]"
+				>Sign up</button
+			>
+			<h2 class="text-center uppercase mb-2">or</h2>
+			<button class="w-full border py-[6px] rounded mb-2 font-semibold hover:opacity-[80%]"
+				>Sign up with Google</button
+			>
+
+			<div class="text-center">
+				<p>
+					Already have an account? <span class="text-primary"><a href=" ">Login</a></span>
+				</p>
+			</div>
+		</form>
+	</div>
+</section>
+
+<style>
+</style>
